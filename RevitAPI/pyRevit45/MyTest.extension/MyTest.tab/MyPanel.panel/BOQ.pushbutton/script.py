2709964e0407c9a6423c783e1926e9c2cc2ff311
MarcusJones <jones.0bj3@gmail.com>
2018-02-05 19:42:29 +0100

    BOQ Progress

--- RevitAPI/pyRevit45/MyTest.extension/MyTest.tab/MyPanel.panel/BOQ.pushbutton/script.py
+++ RevitAPI/pyRevit45/MyTest.extension/MyTest.tab/MyPanel.panel/BOQ.pushbutton/script.py
@@ -69,67 +69,70 @@ def test_get_elements():
     
     print("First element:", elems[0], type(elems[0]))
 
+def get_el_counts():
+    
+    def quant_print(cat, this_dict): 
+        print("{} {}".format(cat, len(this_dict[cat])))
+
+    quant_print("Walls", sorted_elems)
+    quant_print("Floors" , sorted_elems)
+    quant_print("Structural Framing", sorted_elems)
+    quant_print("Generic Models", sorted_elems)
+    quant_print("Curtain Systems", sorted_elems)
+    quant_print("Doors", sorted_elems)
+    quant_print("Columns", sorted_elems)
+    quant_print("Roofs", sorted_elems)
+    quant_print("Windows", sorted_elems)
+    quant_print("Structural Columns", sorted_elems)
+    quant_print("Levels", sorted_elems)
+
+def print_el_table():
+    pass
+
+    #util_params.table_parameters_scirpted(first_wall,this_script.output.print_code)
+
+def print_sorted_elems(sorted_elems):
+    for k in sorted_elems:
+        print(k, len(sorted_elems[k]))
+
+def get_walls():
+    el = sorted_elems["Floors"][0]
+    
+    print(el)
+    print("Name {}".format(el.Name))
+    
+        
+    #first_wall = sorted_elems["Walls"][0]
+    
+    this_dict = util_params.get_all_params(first_wall)
+    #print(first_wall.Name())
+    print(this_dict)
+    
+    print(dir(first_wall))
+    
+#util_params.print_all_params(first_wall)
+    
+    
 #===============================================================================
 #--- MAIN SCRIPT
 #===============================================================================
+
 print("Doc", doc)
 print("uidoc", uidoc)
 
+
 print("Get sorted elements")
 
+
 sorted_elems = util_elems.get_sort_all_elements(doc)
-for k in sorted_elems:
-    break
-    print(k, len(sorted_elems[k]))
-
-def quant_print(cat, this_dict): 
-    print("{} {}".format(cat, len(this_dict[cat])))
-    
-
-quant_print("Walls", sorted_elems)
-quant_print("Floors" , sorted_elems)
-quant_print("Structural Framing", sorted_elems)
-quant_print("Generic Models", sorted_elems)
-quant_print("Curtain Systems", sorted_elems)
-quant_print("Doors", sorted_elems)
-quant_print("Columns", sorted_elems)
-quant_print("Roofs", sorted_elems)
-quant_print("Windows", sorted_elems)
-quant_print("Structural Columns", sorted_elems)
-quant_print("Levels", sorted_elems)
-
-first_wall = sorted_elems["Walls"][0]
-print(first_wall)
-util_params.table_parameters(first_wall)
-
-util_params.table_parameters_sameline(first_wall)
 
 
-print("{:20}".format("-name-").encode('utf-8'), end="")
-print("{:20}".format("-ParameterGroup-").encode('ascii'), end="")
-print("{:30}".format("-ParameterType-").encode('ascii'), end="")
-print("{:30}".format("-Value String-").encode('ascii'), end="")
-print("{:30}".format("-String-").encode('ascii'), end="")
-print("{:30}".format("-UnitType-").encode('ascii'), end="")
-print("")
+boq_elems = util_elems.get_BOQ_elements(doc)
+
+print(len(boq_elems))
 
 
-print("{:20}".format("-name-"), end="")
-print("{:20}".format("-ParameterGroup-"), end="")
-print("{:30}".format("-ParameterType-"), end="")
-print("{:30}".format("-Value String-"), end="")
-print("{:30}".format("-String-"), end="")
-print("{:30}".format("-UnitType-"), end="")
-print("{} {}".format(1,23))
-print("")
-
-
-# prints contents formatted as a python script
-python_script = """
-import sys
-print(sys.paths)
-"""
-this_script.output.print_code(python_script)
+#get_walls()
 
 print("DONE")
 
